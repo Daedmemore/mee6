aba178a405db8f5b469c065faee2755a70860476
Cookie <cookie.io@openmailbox.org>
2016-03-31 11:51:10 +0200

    Enhancing the find_server funct

--- bot/utils.py
+++ bot/utils.py
@@ -1,8 +1,14 @@
+from discord import Server
+
 def find_server(*args, **kwargs):
     for arg in args:
-        if hasattr(arg, 'server'):
+        if isinstance(arg, Server):
+            return arg
+        elif hasattr(arg, 'server'):
             return arg.server
     for key, value in kwargs.items():
-        if hasattr(value, 'server'):
+        if isinstance(arg, Server):
+            return value
+        elif hasattr(value, 'server'):
             return arg.server
     return None
