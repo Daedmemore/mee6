0b7b3b2300b31d4acb4f0eb232356a87c019571f
cookie <cookie.io@openmailbox.org>
2016-04-06 09:10:31 +0200

    Fixed the DEBUG bug

--- bot/bot.py
+++ bot/bot.py
@@ -2,14 +2,13 @@ from mee6 import Mee6
 import os
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
 token = os.getenv('MEE6_TOKEN')
 redis_url = os.getenv('REDIS_URL')
 mee6_debug = os.getenv('MEE6_DEBUG')
-
 if mee6_debug:
     logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
 
 bot = Mee6(redis_url=redis_url)
 bot.run(token)
