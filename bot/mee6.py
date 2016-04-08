9d1370b6794e333eb09dd5c7138a0cb8401404f9
cookie <cookie.io@openmailbox.org>
2016-04-08 17:36:53 +0200

    Droping the dispatch patch for a more robust way

--- bot/mee6.py
+++ bot/mee6.py
@@ -3,7 +3,6 @@ import asyncio
 import logging
 from plugin_manager import PluginManager
 from database import Db
-from utils import find_server
 from time import time
 
 from plugins.commands import Commands
@@ -15,6 +14,12 @@ from plugins.logs import Logs
 from plugins.git import Git
 from plugins.streamers import Streamers
 from plugins.reddit import Reddit
+
+# Global plugins
+from plugins.basiclogs import BasicLogs
+from plugins.stats import Stats
+from plugins.asciiwelcome import AsciiWelcome
+from plugins.mee6game import Mee6Game
 
 log = logging.getLogger('discord')
 
@@ -28,7 +33,7 @@ class Mee6(discord.Client):
     def __init__(self, *args, **kwargs):
         discord.Client.__init__(self, *args, **kwargs)
         self.redis_url = kwargs.get('redis_url')
-        self.db = Db(self.redis_url)
+        self.db = Db(self.redis_url, self.loop)
         self.plugin_manager = PluginManager(self)
         self.plugin_manager.load_all()
         self.last_messages = []
@@ -36,27 +41,27 @@ class Mee6(discord.Client):
     async def on_ready(self):
         """Called when the bot is ready.
 
-        Launched heartbeat, update_stats cron jobs.
-        Change the played game to http://mee6.xyz
+        Launched heartbeat, update_stats cron jobs
+        Connects to the database
+        Dispatched all the ready events
 
         """
-        with open('welcome_ascii.txt') as f:
-            print(f.read())
-        self.add_all_servers()
+        log.info('Connected to the database')
+        await self.add_all_servers()
         discord.utils.create_task(self.heartbeat(5), loop=self.loop)
-        discord.utils.create_task(self.update_stats(60), loop=self.loop)
-        await self.change_status(game=discord.Game(name='http://mee6.xyz'))
+        for plugin in self.plugins:
+            self.loop.create_task(plugin.on_ready())
 
-    def add_all_servers(self):
+    async def add_all_servers(self):
         """Syncing all the servers to the DB"""
         log.debug('Syncing servers and db')
-        self.db.redis.delete('servers')
+        await self.db.redis.delete('servers')
         for server in self.servers:
             log.debug('Adding server {}\'s id to db'.format(server.id))
-            self.db.redis.sadd('servers', server.id)
-            self.db.redis.set('server:{}:name'.format(server.id), server.name)
+            await self.db.redis.sadd('servers', server.id)
+            await self.db.redis.set('server:{}:name'.format(server.id), server.name)
             if server.icon:
-                self.db.redis.set('server:{}:icon'.format(server.id), server.icon)
+                await self.db.redis.set('server:{}:icon'.format(server.id), server.icon)
 
     async def on_server_join(self, server):
         """Called when joining a new server
@@ -67,10 +72,10 @@ class Mee6(discord.Client):
         """
         log.info('Joined {} server : {} !'.format(server.owner.name, server.name))
         log.debug('Adding server {}\'s id to db'.format(server.id))
-        self.db.redis.sadd('servers', server.id)
-        self.db.redis.set('server:{}:name'.format(server.id), server.name)
+        await self.db.redis.sadd('servers', server.id)
+        await self.db.redis.set('server:{}:name'.format(server.id), server.name)
         if server.icon:
-            self.db.redis.set('server:{}:icon'.format(server.id), server.icon)
+            await self.db.redis.set('server:{}:icon'.format(server.id), server.icon)
 
     async def on_server_remove(self, server):
         """Called when leaving or kicked from a server
@@ -80,12 +85,12 @@ class Mee6(discord.Client):
         """
         log.info('Leaving {} server : {} !'.format(server.owner.name, server.name))
         log.debug('Removing server {}\'s id from the db'.format(server.id))
-        self.db.redis.srem('servers', server.id)
+        await self.db.redis.srem('servers', server.id)
 
     async def heartbeat(self, interval):
         """Sends a heartbeat to the db every interval seconds"""
         while self.is_logged_in:
-            self.db.redis.set('heartbeat', 1, ex=interval)
+            await self.db.redis.set('heartbeat', 1, expire=interval)
             await asyncio.sleep(0.9 * interval)
 
     async def update_stats(self, interval):
@@ -95,128 +100,121 @@ class Mee6(discord.Client):
             members = list(self.get_all_members())
             online_members = filter(lambda m: m.status is discord.Status.online, members)
             online_members = list(online_members)
-            self.db.redis.set('mee6:stats:online_members', len(online_members))
-            self.db.redis.set('mee6:stats:members', len(members))
+            await self.db.redis.set('mee6:stats:online_members', len(online_members))
+            await self.db.redis.set('mee6:stats:members', len(members))
 
             # Last messages
             for index, timestamp in enumerate(self.last_messages):
                 if timestamp + interval < time():
                     self.last_messages.pop(index)
-            self.db.redis.set('mee6:stats:last_messages', len(self.last_messages))
+            await self.db.redis.set('mee6:stats:last_messages', len(self.last_messages))
 
             await asyncio.sleep(interval)
 
-    async def _run_plugin_event(self, plugin, event, *args, **kwargs):
-        """A modified coro that is based on Client._run_event"""
-        try:
-            await getattr(plugin, event)(*args, **kwargs)
-        except asyncio.CancelledError:
-            pass
-        except Exception:
-            try:
-                await self.on_error(event, *args, **kwargs)
-            except asyncio.CancelledError:
-                pass
+    async def get_plugins(self, server):
+        plugins = await self.plugin_manager.get_all(server)
+        return plugins
 
     async def on_message(self, message):
-        """Called when a new message is recieved"""
+        server = message.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_message(message))
 
-        # Some stats
-        self.db.redis.incr('mee6:stats:messages')
-        self.last_messages.append(time())
+    async def on_message_edit(self, before, after):
+        server = after.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_message_edit(before, after))
 
-        # Logs all mee6's messages
-        if message.author.id == self.user.id:
-            destination = message.channel
-            if destination.is_private:
-                dest_name = "[PM] {}".format(destination.user.name)
-            else:
-                dest_name = "{}@{}".format(destination.server.name, destination.name)
-            log.info("OUT >> {} >> {}".format(dest_name, message.clean_content.replace('\n', '~')))
+    async def on_message_delete(self, message):
+        server = message.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_message_delete(message))
 
-        mee6_server_id = "159962941502783488"
-        update_channel_id = "160784396679380992"
-        if message.server is None:
-            return
+    async def on_channel_create(self, channel):
+        server = channel.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_channel_create(channel))
 
-        # Forward the new changelog messages to the server owners
-        if (message.server.id, message.channel.id) == (mee6_server_id, update_channel_id):
-            owners = set(server.owner for server in self.servers)
-            for owner in owners:
-                try:
-                    await self.send_message(
-                        owner,
-                        message.content
-                    )
-                except:
-                    pass
-                await asyncio.sleep(2)
+    async def on_channel_update(self, channel):
+        server = channel.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_channel_update(channel))
 
-    def dispatch(self, event, *args, **kwargs):
-        """A method based on discord.Client.dispatch that also dispatches events to plugins"""
-        # A list of events that are available from the plugins
-        plugin_events = (
-            'message',
-            'ready',
-            'message_delete',
-            'message_edit',
-            'channel_delete',
-            'channel_create',
-            'channel_update',
-            'member_join',
-            'member_remove',
-            'member_update',
-            'server_update',
-            'server_role_create',
-            'server_role_delete',
-            'server_role_update',
-            'voice_state_update',
-            'member_ban',
-            'member_unban',
-            'typing'
-        )
+    async def on_channel_delete(self, channel):
+        server = channel.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_channel_delete(channel))
 
-        log.debug('Dispatching event {}'.format(event))
-        method = 'on_' + event
-        handler = 'handle_' + event
+    async def on_member_join(self, member):
+        server = member.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_member_join(member))
 
-        if hasattr(self, handler):
-            getattr(self, handler)(*args, **kwargs)
+    async def on_member_remove(self, member):
+        server = member.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_member_remove(member))
 
-        if event=='message':
-            self.db.redis.incr('mee6:stats:messages')
-            self.last_messages.append(time())
+    async def on_member_update(self, before, after):
+        server = after.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_member_update(before, after))
 
-        if hasattr(self, method):
-            discord.utils.create_task(self._run_event(method, *args,\
-             **kwargs), loop=self.loop)
+    async def on_server_update(self, before, after):
+        server = after
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_server_update(server))
 
-        if event in plugin_events:
-            server_context = find_server(*args, **kwargs)
+    async def on_server_role_create(self, server, role):
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_server_role_create(server, role))
 
-            # Handle the ready event
-            if event == "ready":
-                all_plugins = self.plugins
-                for plugin in all_plugins:
-                    if hasattr(plugin, method):
-                        discord.utils.create_task(
-                            self._run_plugin_event(
-                                plugin,
-                                method,
-                                *args,
-                                **kwargs
-                            ),
-                            loop=self.loop
-                        )
+    async def on_server_role_delete(self, server, role):
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_server_role_delete(server, role))
 
-            if server_context is None:
-                return
-            # For each plugin that the server has enabled
-            enabled_plugins = self.plugin_manager.get_all(server_context)
-            for plugin in enabled_plugins:
-                if hasattr(plugin, method):
-                    discord.utils.create_task(self._run_plugin_event(\
-                    plugin, method, *args, **kwargs), loop=self.loop)
+    async def on_server_role_update(self, before, after):
+        server = after.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_server_role_update(before, after))
+
+    async def on_voice_state_update(self, before, after):
+        server = after.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_voice_state_update(before, after))
+
+    async def on_member_ban(self, member):
+        server = member.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_member_ban(member))
+
+    async def on_member_unban(self, member):
+        server = member.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_member_unban(member))
+
+    async def on_typing(self, channel, user, when):
+        server = channel.server
+        enabled_plugins = await self.get_plugins(server)
+        for plugin in enabled_plugins:
+            self.loop.create_task(plugin.on_typing(channel, user, when))
+
 
     def run(self, token):
         """A patch method to enable bot token"""
