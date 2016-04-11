8189b2d5b68f808fdf76ebb4e7ba25752346c3f6
cookie <cookie.io@openmailbox.org>
2016-04-11 20:04:28 +0200

    Removing the unecessary patched run method

--- chat-bot/mee6.py
+++ chat-bot/mee6.py
@@ -268,23 +268,3 @@ class Mee6(discord.Client):
         for plugin in enabled_plugins:
             self.loop.create_task(plugin.on_typing(channel, user, when))
 
-
-    def run(self, token):
-        """A patch method to enable bot token"""
-        self.token = token
-        self.headers['authorization'] = token
-        self._is_logged_in.set()
-        try:
-            self.loop.run_until_complete(self.connect())
-        except KeyboardInterrupt:
-            self.loop.run_until_complete(self.logout())
-            pending = asyncio.Task.all_tasks()
-            gathered = asyncio.gather(*pending)
-            try:
-                gathered.cancel()
-                self.loop.run_forever()
-                gathered.exception()
-            except:
-                pass
-        finally:
-            self.loop.close()
