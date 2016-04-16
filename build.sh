6a977c112eb24f68c5de1fff3bc7db82f8d389e8
cookie <cookie.io@openmailbox.org>
2016-04-16 05:39:42 +0200

    Forcing no-cache rebuilds

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-git pull && cd chat-bot/ && docker build -t mee6-bot . && cd ../website && docker build -t mee6-web . && cd ../
+git pull
+cd chat-bot/
+docker build --no-cache -t mee6-bot .
+cd ../website
+docker build --no-cache -t mee6-web .
+cd ../
