fb6db20ffdf156ddac96dacb40dac02694192ef1
cookie <cookie.io@openmailbox.org>
2016-04-29 22:09:28 +0200

    Adding shard support

--- chat-bot/bot.py
+++ chat-bot/bot.py
@@ -6,10 +6,12 @@ token = os.getenv('MEE6_TOKEN')
 redis_url = os.getenv('REDIS_URL')
 mongo_url = os.getenv('MONGO_URL')
 mee6_debug = os.getenv('MEE6_DEBUG')
+shard = int(os.getenv('SHARD'))
+shard_count = int(os.getenv('SHARD_COUNT'))
 if mee6_debug:
     logging.basicConfig(level=logging.DEBUG)
 else:
     logging.basicConfig(level=logging.INFO)
 
-bot = Mee6(redis_url=redis_url, mongo_url=mongo_url)
+bot = Mee6(shard_id=shard, shard_count=shard_count, redis_url=redis_url, mongo_url=mongo_url)
 bot.run(token)
